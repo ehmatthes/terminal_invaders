07adfcbc00f5aa2c05de70b2b610256a32e3a69d
Eric Matthes <ehmatthes@gmail.com>
2013-10-16 22:17:02 -0800

    Initial commit.

---
+++ terminal_invaders.py
@@ -0,0 +1,35 @@
+import os
+# Left arrow is 68, right arrow is 67, space is 32
+import getch
+
+# Terminal Invaders
+#
+# A terminal-based clone of Space Invaders.
+#  Eric Matthes
+#  @ehmatthes
+
+ship_string = '|-/\-|'
+
+def draw_ship(x_pos):
+    y_spacing = '\n\n\n'
+    x_spacing = ''
+    for x in range(0, x_pos):
+        x_spacing += ' '
+    print(y_spacing + x_spacing + ship_string)
+
+    # Move cursor down, out of the way
+    print("\n\n\n\n\n")
+
+
+x_ship = 0
+
+os.system('clear')
+print("Terminal Invaders - press 'q' to quit")
+draw_ship(x_ship)
+
+input = getch.getch()
+while input != 'q':
+    os.system('clear')
+    print("Terminal Invaders - press 'q' to quit")
+    draw_ship(int(input))
+    input = getch.getch()
